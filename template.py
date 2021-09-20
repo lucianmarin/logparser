5c41205c0ac45954fe805b92aa65c2866386f17c
Lucian Marin <marin.lucian@gmail.com>
2021-09-20 14:12:19 +0300

    display number of items

--- template.py
+++ template.py
@@ -3,14 +3,18 @@ TEMPLATE = """<!doctype html>
 <head>
     <title>Logparser</title>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, height=device-height, initial-scale=1, user-scalable=0">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <style>
         * { margin: 0; padding: 0; }
         body {
-            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+            font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI',
+                Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans',
+                'Helvetica Neue', sans-serif;
             font-size: 13px;
             line-height: 20px;
         }
+        sup { line-height: 0; }
+        h3 sup { font-size: 11px; margin-left: 1px; }
         .section {
             margin: 0 auto;
             max-width: 480px;
@@ -33,28 +37,37 @@ TEMPLATE = """<!doctype html>
 </head>
 <body>
     <div class="section">
-        <h3>Days</h3>
-        {% for day, ips in days | reverse -%}
-            <p class="{{ loop.cycle('odd', 'even') }}"><span>{{ ips | length }}</span> {{ day }}</p>
-        {%- endfor %}
+        <h3>Days<sup>{{ days | length }}</sup></h3>
+        {% for day, ips in days | reverse %}
+            <p class="{{ loop.cycle('odd', 'even') }}">
+                <span>{{ ips | length }}</span> {{ day }}
+            </p>
+        {% endfor %}
     </div>
     <div class="section">
-        <h3>Browsers</h3>
-        {% for browser, ips in browsers | reverse -%}
-            <p class="{{ loop.cycle('odd', 'even') }}"><span>{{ ips | length }}</span> {{ browser }}</p>
-        {%- endfor %}
+        <h3>Browsers<sup>{{ browsers | length }}</sup></h3>
+        {% for browser, ips in browsers | reverse %}
+            <p class="{{ loop.cycle('odd', 'even') }}">
+                <span>{{ ips | length }}</span> {{ browser }}
+            </p>
+        {% endfor %}
     </div>
     <div class="section">
-        <h3>Operating Systems</h3>
-        {% for os, ips in oses | reverse -%}
-            <p class="{{ loop.cycle('odd', 'even') }}"><span>{{ ips | length }}</span> {{ os }}</p>
-        {%- endfor %}
+        <h3>Operating Systems<sup>{{ oses | length }}</sup></h3>
+        {% for os, ips in oses | reverse %}
+            <p class="{{ loop.cycle('odd', 'even') }}">
+                <span>{{ ips | length }}</span> {{ os }}
+            </p>
+        {% endfor %}
     </div>
     <div class="section">
-        <h3>Referrers</h3>
-        {% for ref, ips in refs | reverse -%}
-            <p class="{{ loop.cycle('odd', 'even') }}"><span>{{ ips | length }}</span> {{ ref }}</p>
-        {%- endfor %}
+        <h3>Referrers<sup>{{ refs | length }}</sup></h3>
+        {% for ref, ips in refs | reverse %}
+            <p class="{{ loop.cycle('odd', 'even') }}">
+                <span>{{ ips | length }}</span> {{ ref }}
+            </p>
+        {% endfor %}
     </div>
 </body>
-</html>"""
+</html>
+"""
