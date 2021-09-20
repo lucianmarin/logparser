44b5f119bf1ba1c91dcf0e654ea7b142161abba7
Lucian Marin <marin.lucian@gmail.com>
2021-09-20 09:14:10 +0300

    generate html using jinja2

--- parse.py
+++ parse.py
@@ -4,55 +4,71 @@ from urllib.parse import urlparse
 
 from clfparser import CLFParser
 from fire import Fire
+from jinja2 import Environment, FileSystemLoader
 from tqdm import tqdm
 from user_agents import parse as uaparse
 
 SKIP_REFS = ["subreply.com", "unfeeder.com"]
 
 
-def parse(gz_path):
-    ips = defaultdict(set)
+def generate_html(days, browsers, oses, refs, html):
+    print('Generating HTML...', html)
+    env = Environment(loader=FileSystemLoader("templates"))
+    template = env.get_template('basic.html')
+    with open(html, 'w') as file:
+        output = template.render(
+            title=html,
+            days=sorted(days.items()),
+            oses=sorted(oses.items(), key=lambda item: len(item[1])),
+            browsers=sorted(browsers.items(), key=lambda item: len(item[1])),
+            refs=sorted(refs.items(), key=lambda item: len(item[1]))
+        )
+        file.write(output)
+
+
+def console_print(days, browsers, oses, refs):
+    print('----- Day')
+    for k, v in sorted(days.items()):
+        print(str(len(v)).rjust(5), k)
+    print('\n----- OS')
+    for k, v in sorted(oses.items(), key=lambda item: len(item[1])):
+        print(str(len(v)).rjust(5), k)
+    print('\n----- Browser')
+    for k, v in sorted(browsers.items(), key=lambda item: len(item[1])):
+        print(str(len(v)).rjust(5), k)
+    print('\n----- Referer')
+    for k, v in sorted(refs.items(), key=lambda item: len(item[1])):
+        print(str(len(v)).rjust(5), k)
+
+
+def parse(gz_path, html=None):
+    days = defaultdict(set)
     browsers = defaultdict(set)
     oses = defaultdict(set)
     refs = defaultdict(set)
-
-    with gzip.open(gz_path, "rt") as file:
-        for line in tqdm(file):
+    with gzip.open(gz_path, 'rt') as file:
+        for line in tqdm(file, unit=""):
             log = CLFParser.logDict(line)
             ip = log['h']
             day = log['time'].strftime('%Y-%m-%d %a')
-            ips[day].add(ip)
-
+            days[day].add(ip)
             ua_str = log["Useragent"][1:-1]
             ua_parsed = uaparse(ua_str)
             if not ua_parsed.is_bot:
                 browsers[ua_parsed.browser.family].add(ip)
                 oses[ua_parsed.os.family].add(ip)
-
             ref = log["Referer"][1:-1].lower()
-            ref = ref.replace("://www.", "://")
+            ref = ref.replace('://www.', '://')
             p = urlparse(ref)
             if p.path.endswith('/'):
                 p = p._replace(path=p.path[:-1])
             ref = f"{p.netloc}{p.path}"
             if ref != "-" and not any(s in ref for s in SKIP_REFS):
                 refs[ref].add(ip)
-
-    print('----- IP')
-    for k, v in sorted(ips.items()):
-        print(str(len(v)).rjust(5), k)
-
-    print('\n----- OS')
-    for k, v in sorted(oses.items(), key=lambda item: len(item[1])):
-        print(str(len(v)).rjust(5), k)
-
-    print('\n----- Browser')
-    for k, v in sorted(browsers.items(), key=lambda item: len(item[1])):
-        print(str(len(v)).rjust(5), k)
-
-    print('\n----- Referer')
-    for k, v in sorted(refs.items(), key=lambda item: len(item[1])):
-        print(str(len(v)).rjust(5), k)
+    if html:
+        generate_html(days, browsers, oses, refs, html)
+    else:
+        console_print(days, browsers, oses, refs)
 
 
 if __name__ == "__main__":
